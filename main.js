ef8407635d263c75832eac45698d3145bf841a53
ccrs11 <ccrs11@hotmail.com>
2023-05-16 17:29:42 -0500

    feat: :rocket: iniciar Proyecto de mejoramiento

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ async function traercomentarios(id) {
     const insertComment = document.querySelector('.comentarios');
     let comment = "";
     let avatar = "";
-    insertComment.innerHTML=``;
+    insertComment.innerHTML = ``;
     for (let i = 0; i < 20; i++) {
       comment = result.comments[i].content;
       avatar = result.comments[i].author.avatar[0].url;
@@ -44,16 +44,16 @@ async function traerDescription(id) {
     const response = await fetch(url, options);
     const result = await response.json();
     //console.log(result);
-    const description=result.description;
+    const description = result.description;
     //console.log(description);
     const insertDescription = document.querySelector(".descripcion");
-    insertDescription.innerHTML=`<p class="descripcion" >${description}</p>`;
+    insertDescription.innerHTML = `<p class="descripcion" >${description}</p>`;
   } catch (error) {
     console.error(error);
   }
 }
 
-async function traerRelatedVideos(id){
+async function traerRelatedVideos(id) {
   const url = `https://youtube138.p.rapidapi.com/video/related-contents/?id=${id}&hl=en&gl=US`;
   const options = {
     method: 'GET',
@@ -62,15 +62,15 @@ async function traerRelatedVideos(id){
       'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
     }
   };
-  
+
   try {
     const response = await fetch(url, options);
     const result = await response.json();
-/*     console.log(result); */
+    /*     console.log(result); */
 
     const insertRelated = document.querySelector('.relatedV');
     let idvideo = "";
-    insertRelated.innerHTML="";
+    insertRelated.innerHTML = "";
     for (let i = 0; i < 10; i++) {
       idvideo = result.contents[i].video.videoId;
       insertRelated.insertAdjacentHTML("beforeend", `<iframe width="150rem" height="150rem" src="https://www.youtube.com/embed/${idvideo}" frameborder="0" allowfullscreen></iframe>`);
@@ -83,30 +83,30 @@ async function traerRelatedVideos(id){
   }
 }
 
-async function nombreCanal(channelId){
+async function nombreCanal(channelId) {
   const url = `https://youtube138.p.rapidapi.com/channel/details/?id=${channelId}&hl=en&gl=US`;
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '91c5acf63cmshdf8666df3c954c3p19b287jsn031880e1823c',
-		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-	}
-};
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '91c5acf63cmshdf8666df3c954c3p19b287jsn031880e1823c',
+      'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+    }
+  };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-	//console.log(result);
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    //console.log(result);
 
-  const insertName = document.querySelector('.nombreCanal');
-  let nombre = "";
-  let avatar = "";
+    const insertName = document.querySelector('.nombreCanal');
+    let nombre = "";
+    let avatar = "";
     nombre = result.title;
     avatar = result.avatar[0].url;
-    insertName.innerHTML=`<div><img src="${avatar}" alt="avatar"> <p>${nombre}</p><div>`;
-} catch (error) {
-	console.error(error);
-}
+    insertName.innerHTML = `<div><img src="${avatar}" alt="avatar"> <p>${nombre}</p><div>`;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 
